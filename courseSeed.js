5dbf2e9ee7a375e699ddf0cf43da41edc50bd7c5
joycelin12 <joycelin12@hotmail.com>
2019-09-29 16:03:49 -0400

    to correct courseSeed.js

--- courseSeed.js
+++ courseSeed.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose"),
      mongoose.Promise = global.Promise;
      mongoose.connect(
 	process.env.MONGO_URI || "mongodb://localhost:27017/recipe_db",
-	     {useNewUrlParse: true}
+	     {useNewUrlParser: true}
 
      );
      Course.remove({}) //remove all existing documents
