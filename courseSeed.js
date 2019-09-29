b8debc3cbd240fc82302b835c8eba5710cfe24f7
joycelin12 <joycelin12@hotmail.com>
2019-09-29 15:57:01 -0400

    to add courses to app

---
+++ courseSeed.js
@@ -0,0 +1,51 @@
+"use strict";
+
+const mongoose = require("mongoose"),
+	Course = require("./models/course"); //require models for seeding data
+
+     mongoose.Promise = global.Promise;
+     mongoose.connect(
+	process.env.MONGO_URI || "mongodb://localhost:27017/recipe_db",
+	     {useNewUrlParse: true}
+
+     );
+     Course.remove({}) //remove all existing documents
+          .then(() =>
+		  return Course.create({
+			  title: "Beets sitting at home",
+			  description: "Seasonal beets from the guy down the street.",
+			  zipCode: 12323,
+			  items: ["beets"]
+		  });
+	    })
+          .then(course => console.log(course.title))
+          .then(() => {
+		return Course.create({
+			title: "Barley even listening",
+			description: "Organic wheats and barleys for bread, soup, and fun!",
+			zipCode: 20325,
+			items: ["barley", "rye", "wheat"]
+			
+	  	});
+	  })
+          .then(course => console.log(course.title))
+          .then(() => {
+		return Course.create({
+			title: "Peaching to the choir",
+			description: "Get fresh peaches from the local farm",
+			zipCode: 10065,
+			items: ["peaches", "plums"]
+			
+	  	});
+	  })
+          .then(course => console.log(course.title))
+          .catch(error => console.log(error.message))
+          .then(() => 
+		  console.log("DONE");
+		  mongoose.connection.close();
+	   });
+
+     
+      
+
+    
